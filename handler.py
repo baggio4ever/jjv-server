be6df09709e30f7ec5cfb6c409e433a7c1ee6ba7
baggio4ever <baggio4ever@gmail.com>
2018-03-13 22:30:56 +0900

    dynamoDBへの書き込みサンプル

--- handler.py
+++ handler.py
@@ -1,7 +1,18 @@
 import json
+from datetime import datetime
+import boto3
+from boto3.dynamodb.conditions import Key,Attr
 
 
 def hello(event, context):
+    if "headers" in event:
+        if "origin" in event["headers"]:
+            origin = event["headers"]["origin"]  # どこから聞かれても返せるように
+        else:
+            origin = ""
+    else:
+        origin = ""
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
@@ -9,7 +20,10 @@ def hello(event, context):
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(body),
+        "headers": {
+            "Access-Control-Allow-Origin":origin
+        }
     }
 
     return response
@@ -24,14 +38,51 @@ def hello(event, context):
     """
 
 def post_log(event, context):
+    if "headers" in event:
+        if "origin" in event["headers"]:
+            origin = event["headers"]["origin"]  # どこから聞かれても返せるように
+        else:
+            origin = ""
+    else:
+        origin = ""
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
 
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('dev-jjv')
+
+    #yy = json.loads(event["body"])
+
+    user_id = "taro" #yy["user_id"]
+    dt = datetime.now().strftime('%Y/%m/%d %H:%M:%S.%f')
+    message = "hello!!" #yy["message"]
+
+    table.put_item(
+        Item={
+            "user_id": user_id,
+            "created_at": dt,
+            "message": message
+        }
+    )
+
+    xx = json.loads(
+        '''
+        {
+            "user_id":"taro",
+            "message":"よこはまたそがれ"
+        }
+        '''
+    )
+
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(body),
+        "headers": {
+            "Access-Control-Allow-Origin":origin
+        }
     }
 
     return response
