d449219a3b3421fcc6035911ccbc9fcde0efa41e
baggio4ever <baggio4ever@gmail.com>
2018-03-15 12:13:04 +0900

    upload機能実装始めました

--- handler.py
+++ handler.py
@@ -88,3 +88,57 @@ def post_log(event, context):
     }
 
     return response
+
+def upload(event, context):
+    if "headers" in event:
+        if "origin" in event["headers"]:
+            origin = event["headers"]["origin"]  # どこから聞かれても返せるように
+        else:
+            origin = ""
+    else:
+        origin = ""
+
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('dev-jjv')
+
+    yy = json.loads(event["body"])
+
+    user_id = yy["user_id"]
+    dt = datetime.now().strftime('%Y/%m/%d %H:%M:%S.%f')
+    filename = yy["filename"]
+    xml = yy["xml"]
+    comment = yy["comment"]
+
+    table.put_item(
+        Item={
+            "user_id": user_id,
+            "created_at": dt,
+            "filename": filename,
+            "xml": xml,
+            "comment": comment
+        }
+    )
+
+    xx = json.loads(
+        '''
+        {
+            "user_id":"taro",
+            "message":"よこはまたそがれ"
+        }
+        '''
+    )
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        "headers": {
+            "Access-Control-Allow-Origin":origin
+        }
+    }
+
+    return response
